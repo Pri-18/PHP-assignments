e71cde3f1ca389bb4e1bc620809e58c3f936e8d9
Pri-18 <priyansu.bhattacharya@innoraft.com>
2025-03-27 11:10:47 +0530

    Implement changes as reviewed

--- form.php
+++ form.php
@@ -4,35 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form result</title>
-    <style>
-        h1 {
-            font-size: 4rem;
-            text-align: center;
-            margin-top: 4rem;
-            color: green;
-        }
-    </style>
+    <link rel="stylesheet" href="form.css">
 </head>
 <body>
     <?php
-    class User {
-        public $firstName;
-        public $lastName;
+/**
+ * Class User
+ * 
+ * Provides an user's first and last name.
+ */
+class User {
+  /**
+   * First name of the user.
+   *
+   * @var string
+   */
+  public $firstName;
 
-        public function __construct($firstName, $lastName) {
-            $this->firstName = trim($firstName);
-            $this->lastName = trim($lastName);
-        }
+  /**
+   * Last name of the user.
+   *
+   * @var string
+   */
+  public $lastName;
 
-        public function getFullName() {
-            return "$this->firstName" ." " . "$this->lastName";
-        }
-    }
+  /**
+   * Constructs a new User object.
+   *
+   * @param string $firstName
+   * @param string $lastName
+   */
+  public function __construct($firstName, $lastName) {
+    // Remove extra whitespace.
+    $this->firstName = trim($firstName);
+    $this->lastName = trim($lastName);
+  }
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $user = new User($_POST['fname'], $_POST['lname']);
-        echo "<h1>Hello {$user->getFullName()}</h1>";
-    }
-    ?>
+  /**
+   * Returns the full name of the user.
+   *
+   * @return string
+   */
+  public function getFullName() {
+    return "$this->firstName $this->lastName";
+  }
+}
+
+// Check the request method (POST).
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  /**
+   * Create a new User object.
+   *
+   * @var User $user
+   */
+  $user = new User($_POST['fname'], $_POST['lname']);
+
+  // Display the message.
+  echo "<h1>Hello {$user->getFullName()}</h1>";
+}
+?>
+</body>
+</html>
+
 </body>
 </html>
